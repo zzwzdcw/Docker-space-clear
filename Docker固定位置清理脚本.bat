bcb884223d77335a039b14d5d587976b985735fa
zzwzdcw <2749978755@qq.com>
2021-06-11 12:24:35 +0800

    固定位置清理脚本可以基本完成常用的功能

--- Docker固定位置清理脚本.bat
+++ Docker固定位置清理脚本.bat
@@ -1,9 +1,38 @@
 @echo off
+:start
+echo 请选择删除选项
+echo 1.删除所有未被 tag 标记和未被容器使用的镜像
+echo 2.删除所有未被容器使用的镜像
+echo 3.删除所有停止运行的容器
+echo 4.删除所有未被挂载的卷
+echo 5.删除所有网络
+echo 6.删除 docker 所有资源
+echo ！！！务必保证选择正确，不然会自动退出！！！
+set /p c=
+if %c% ==1 (
+docker image prune
+)else if %c% ==2 (
+docker image prune -a
+)else if %c% ==3 (
+docker container prune
+)else if %c% ==4 (
+docker volume prune
+)else if %c% ==5 (
+docker network prune
+)else if %c% ==6 (
+docker system prune
+)esle (
+goto start
+)
+:main
+echo 删除完成正在为您退出Docker Desktop.exe
+taskkill /f /im "Docker Desktop.exe"
+echo 正在为您退出wsl...
 wsl --shutdown
-echo close wsl.....
-::-----------------璇峰皢vdisk file鏂囦欢浣嶇疆鎹㈡垚鎮ㄨ嚜宸辩殑浣嶇疆---------------------------------------
+echo 开始清理占用的空间
+::-----------------请将vdisk file文件位置换成您自己的位置---------------------------------------
 (echo select vdisk file="C:\Users\zzwzd\AppData\Local\Docker\wsl\data\ext4.vhdx"
-::-----------------璇峰皢vdisk file鏂囦欢浣嶇疆鎹㈡垚鎮ㄨ嚜宸辩殑浣嶇疆---------------------------------------
+::-----------------请将vdisk file文件位置换成您自己的位置---------------------------------------
 echo attach vdisk readonly
 echo compact vdisk
 echo detach vdisk) | diskpart 
---
+++ 新建文本文档.bat
@@ -0,0 +1,9 @@
+:iii
+echo iii
+goto ii
+:ii
+echo ii
+goto i
+:i
+echo i
+pause
